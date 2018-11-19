5552a35cb108f75227c6f27d68612e4f02364637
Luc <luc.lemahieu@laposte.net>
2018-11-19 16:59:00 +0100

    preparation et debut des exos

---
+++ exercice1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Exercice 1 php Partie 5</title>
+    </head>
+    <body>
+        <?php
+            $month = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre');
+        ?>
+    </body>
+</html>
